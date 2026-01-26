fdd0e2eeaff36064c8a8cb51c51de865817611bb
STAAAR-MAX <aliaripidi@gmail.com>
2026-01-26 10:32:36 +0700

    Update pro_load_bronze.sql

--- Scripts/bronze/pro_load_bronze.sql
+++ Scripts/bronze/pro_load_bronze.sql
@@ -17,8 +17,8 @@
 -- EXEC bronze.load_bronze;
 -- ===============================================================================================================
 
-EXEC bronze.load_bronze
-
+EXEC bronze.load_bronze;
+	
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN 
 	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
@@ -38,7 +38,7 @@ BEGIN
 
 		PRINT '>> Inserting Table: bronze.crm_cust_info';
 		BULK INSERT bronze.crm_cust_info
-		FROM 'D:\belajar\sql-ultimate-course-main\project\sql-data-warehouse-project-main\datasets\source_crm\cust_info.csv'
+		FROM 'D:\belajar\SQL\project\sql data warehouse\datasets\source_crm\cust_info.csv'
 		WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR=',',
@@ -55,7 +55,7 @@ BEGIN
 	
 		PRINT '>> Inserting Table: bronze.crm_prd_info';
 		BULK INSERT bronze.crm_prd_info
-		FROM 'D:\belajar\sql-ultimate-course-main\project\sql-data-warehouse-project-main\datasets\source_crm\prd_info.csv'
+		FROM 'D:\belajar\SQL\project\sql data warehouse\datasets\source_crm\prd_info.csv'
 		WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR=',',
@@ -71,7 +71,7 @@ BEGIN
 
 		PRINT '>> Inserting Table: bronze.crm_sales_details';
 		BULK INSERT bronze.crm_sales_details
-		FROM 'D:\belajar\sql-ultimate-course-main\project\sql-data-warehouse-project-main\datasets\source_crm\sales_details.csv'
+		FROM 'D:\belajar\SQL\project\sql data warehouse\datasets\source_crm\sales_details.csv'
 		WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR=',',
@@ -91,7 +91,7 @@ BEGIN
 
 		PRINT '>> Inserting Table: bronze.erp_cust_az12';
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'D:\belajar\sql-ultimate-course-main\project\sql-data-warehouse-project-main\datasets\source_erp\cust_az12.csv'
+		FROM 'D:\belajar\SQL\project\sql data warehouse\datasets\source_erp\cust_az12.csv'
 		WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR=',',
@@ -111,7 +111,7 @@ BEGIN
 	
 		PRINT '>> Inserting Table: bronze.erp_loc_a101';
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'D:\belajar\sql-ultimate-course-main\project\sql-data-warehouse-project-main\datasets\source_erp\loc_a101.csv'
+		FROM 'D:\belajar\SQL\project\sql data warehouse\datasets\source_erp\loc_a101.csv'
 		WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR=',',
@@ -127,7 +127,7 @@ BEGIN
 	
 		PRINT '>> Inserting Table: bronze.erp_px_cat_g1v2';
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'D:\belajar\sql-ultimate-course-main\project\sql-data-warehouse-project-main\datasets\source_erp\px_cat_g1v2.csv'
+		FROM 'D:\belajar\SQL\project\sql data warehouse\datasets\source_erp\px_cat_g1v2.csv'
 		WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR=',',
@@ -152,3 +152,4 @@ BEGIN
 		PRINT '====================================================';
 	END CATCH
 END 
+
